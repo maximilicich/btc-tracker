a98c1c41a341afbd8fd69628a4340fe77488ec1d
Maximiliano Milicich <maximilianomilicich@gmail.com>
2022-02-19 22:51:56 -0300

    setup inicial del proyecto usando spring initializr https://start.spring.io/ : Java 8, Maven, Spring Boot 2.5.9 (Spring Web+Spring Data JPA+Spring Reactive+H2 DB)

---
+++ src/main/java/ar/com/wnc/btctracker/BtcTrackerApplication.java
@@ -0,0 +1,13 @@
+package ar.com.wnc.btctracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/wnc/btctracker/BtcTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.wnc.btctracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
